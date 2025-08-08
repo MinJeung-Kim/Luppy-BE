81e1fcd386c717312fdddf5f70b526bd881080ac
MinJeung-Kim <focso5@gmail.com>
2025-08-08 13:22:47 +0900

    Fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,8 @@
 import { Controller, Post, Headers, Body, Request, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
 import { AuthService } from './auth.service';
 import { Public } from './decorator/public.decorator';
-import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
