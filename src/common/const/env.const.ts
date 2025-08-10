339d270ee5b1b12ae3022d80f5e0e8d2ced749e9
MinJeung-Kim <focso5@gmail.com>
2025-08-10 15:04:29 +0900

    Fix: env

--- src/common/const/env.const.ts
+++ src/common/const/env.const.ts
@@ -5,12 +5,6 @@ const dbPort = 'DB_PORT';
 const dbUsername = 'DB_USERNAME';
 const dbPassword = 'DB_PASSWORD';
 const dbDatabase = 'DB_DATABASE';
-// 운영 환경용 DB 설정
-const prodDbHost = 'MYSQLHOST';
-const prodDbPort = 'MYSQLPORT';
-const prodDbUsername = 'MYSQLUSER';
-const prodDbPassword = 'MYSQLPASSWORD';
-const prodDbDatabase = 'MYSQLDATABASE';
 const hashRounds = 'HASH_ROUNDS';
 const accessTokenSecret = 'ACCESS_TOKEN_SECRET';
 const refreshTokenSecret = 'REFRESH_TOKEN_SECRET';
@@ -23,11 +17,6 @@ export const envVariables = {
   dbUsername,
   dbPassword,
   dbDatabase,
-  prodDbHost,
-  prodDbPort,
-  prodDbUsername,
-  prodDbPassword,
-  prodDbDatabase,
   hashRounds,
   accessTokenSecret,
   refreshTokenSecret,
